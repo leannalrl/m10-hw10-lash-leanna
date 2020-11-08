e3f28221708181112eb152bf0cc42c46ce80920d
Leanna <lrlash17@ufl.edu>
2020-11-08 15:47:54 -0500

    Updated main.js with setTimeout and if and else statements.

--- main.js
+++ main.js
@@ -17,16 +17,34 @@ const waterTemp = () =>{
 
 waterTemp();
 
+//Another way to create function that we went over in our one on one.
 
+//function myFunction() {
+//   var element = document.getElementById("boiling");
+//	 if (waterTemp() > 212){
+//		 element.classList.remove("hide");
+//	   element.classList.add("show");
+//	 }
+//}
 
-function myFunction() {
-   var element = document.getElementById("boiling");
-	 if (waterTemp() > 212){
-		 element.className.remove("hide");
-	   element.className.add("show");
-	 }
-}
+//setTimeout(myFunction, 3000);
 
-setTimeout(function(){
-	document.getElementById("boiling").innerHTML;
+var newTemp = 50;
+
+setTimeout(function waterTemps() {
+	if (newTemp > 212){
+		document.getElementById("boiling").classList.remove("hide");
+		console.log (document.getElementById("boiling").classList);
+		document.getElementById("boiling").classList.add("show");
+	}
+	else if (newTemp < 32){
+		document.getElementById("frozen").classList.remove("hide");
+		console.log (document.getElementById("frozen").classList);
+		document.getElementById("frozen").classList.add("show");
+	}
+	else {
+		document.getElementById("good-temp").classList.remove("hide");
+		console.log (document.getElementById("good-temp").classList);
+		document.getElementById("good-temp").classList.add("show");
+	}
 }, 3000);
