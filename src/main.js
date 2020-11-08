0e24203c645d745f33236c5866df8cabe12610c4
Leanna <lrlash17@ufl.edu>
2020-11-08 15:51:32 -0500

    Moved my main.js to src folder

---
+++ src/main.js
@@ -0,0 +1,50 @@
+// It is always helpful to use comments in your code!
+const boilingTemp = () => {
+	document.getElementById("boiling-temp").innerHTML = 212;
+};
+
+boilingTemp();
+
+const freezingTemp = () =>{
+	document.getElementById("freezing-temp").innerHTML = 32;
+};
+
+freezingTemp();
+
+const waterTemp = () =>{
+	document.getElementById("water-temp").innerHTML = 75;
+};
+
+waterTemp();
+
+//Another way to create function that we went over in our one on one.
+
+//function myFunction() {
+//   var element = document.getElementById("boiling");
+//	 if (waterTemp() > 212){
+//		 element.classList.remove("hide");
+//	   element.classList.add("show");
+//	 }
+//}
+
+//setTimeout(myFunction, 3000);
+
+var newTemp = 50;
+
+setTimeout(function waterTemps() {
+	if (newTemp > 212){
+		document.getElementById("boiling").classList.remove("hide");
+		console.log (document.getElementById("boiling").classList);
+		document.getElementById("boiling").classList.add("show");
+	}
+	else if (newTemp < 32){
+		document.getElementById("frozen").classList.remove("hide");
+		console.log (document.getElementById("frozen").classList);
+		document.getElementById("frozen").classList.add("show");
+	}
+	else {
+		document.getElementById("good-temp").classList.remove("hide");
+		console.log (document.getElementById("good-temp").classList);
+		document.getElementById("good-temp").classList.add("show");
+	}
+}, 3000);
